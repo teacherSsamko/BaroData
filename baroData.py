d4397f981494ebd257f976a54cbfc9f434d619e9
ssamko <teacher.ssamko@gmail.com>
2020-04-22 18:32:40 +0900

    first commit

---
+++ baroData.py
@@ -0,0 +1,29 @@
+import csv
+import os
+from os import listdir
+
+list_files = listdir("roll_list/")
+print(list_files)
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+LIST_DIR = os.path.join(BASE_DIR, "roll_list/")
+
+for f in list_files:
+    file_csv = os.path.join(LIST_DIR, f)
+    new_rows = []
+    with open(file_csv, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(reader)
+        for row in reader:
+            print(' '.join(row))
+            if row and row[4] != '':
+                new_line = ','.join([row[4], row[0], row[5]])
+                new_rows.append(new_line)
+
+    with open('sample.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in new_rows:
+            print(row)
+            spamwriter.writerow(row)
+    break
